ed89c465a2d16285d046bf9d79a75cba92d3d8bd
Tyler Yep <tyep@stanford.edu>
2020-06-29 23:06:39 -0700

    travis integration, linkedlist implementation

--- src/structures/linked_list.py
+++ src/structures/linked_list.py
@@ -1,29 +1,73 @@
 from __future__ import annotations
 
-from typing import Any, List, Optional
+from typing import Any, Generic, Iterator, List, Optional, TypeVar
+
+T = TypeVar("T")
 
 
-class LinkedListNode:
-    def __init__(self, data: int, next_node: Optional[LinkedListNode] = None) -> None:
-        self.data = data
-        self.next = next_node
+class LinkedList(Generic[T]):
+    def __init__(self) -> None:
+        self.head: Optional[LinkedListNode[T]] = None
+        self.size = 0
 
-    def __repr__(self) -> str:
-        return f"({self.data}) -> {self.next}"
+    def __len__(self) -> int:
+        return self.size
 
-
-class LinkedList:
-    def __init__(self, lst: Optional[LinkedListNode] = None):
-        self.head = lst
+    def __iter__(self) -> Iterator[LinkedListNode[T]]:
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
 
     @classmethod
-    def from_list(cls, lst: List[Any]) -> LinkedList:
+    def from_list(cls, lst: List[T]) -> LinkedList[T]:
         linked_lst = cls()
-        for item in lst:
-            linked_lst.add(item)
+        for item in reversed(lst):
+            linked_lst.insert(item)
         return linked_lst
 
-    def add(self, data: Any) -> None:
+    def insert(self, data: T, index: int = 0) -> None:
+        """ Inserts data to the front of the list, or at the specified index. """
+        assert index >= 0
+        if index == 0:
+            self.head = LinkedListNode(data, self.head)
+        elif index < self.size:
+            curr = self.head
+            prev = None
+            for i in range(index):
+                prev = curr
+                assert curr is not None
+                curr = curr.next
+            if prev is not None:
+                prev.next = LinkedListNode(data, curr)
+        self.size += 1
+
+    def search(self, data: T) -> bool:
+        curr = self.head
+        while curr is not None:
+            if curr.data == data:
+                return True
+            curr = curr.next
+        return False
+
+    def remove_node(self, data: T) -> None:
+        if self.head is None:
+            raise Exception("List is empty")
+
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+
+        prev = self.head
+        for node in self:
+            if node.data == data:
+                prev.next = node.next
+                return
+            prev = node
+
+        raise Exception("Node not found")
+
+    def add_to_end(self, data: T) -> None:
         if self.head is None:
             self.head = LinkedListNode(data)
             return
@@ -33,10 +77,10 @@ class LinkedList:
             curr = curr.next
         curr.next = LinkedListNode(data)
 
-    def remove_last(self) -> Optional[LinkedListNode]:
+    def remove_last(self) -> Optional[LinkedListNode[T]]:
         """ Deletes the last element of a linked list using only self.head. """
 
-        def _remove_last(lst: Optional[LinkedListNode]) -> Optional[LinkedListNode]:
+        def _remove_last(lst: Optional[LinkedListNode[T]]) -> Optional[LinkedListNode[T]]:
             if lst is None or lst.next is None:
                 return None
             lst.next = _remove_last(lst.next)
@@ -46,3 +90,12 @@ class LinkedList:
 
     def __repr__(self) -> str:
         return str(self.head)
+
+
+class LinkedListNode(Generic[T]):
+    def __init__(self, data: T, next_node: Optional[LinkedListNode[T]] = None) -> None:
+        self.data = data
+        self.next = next_node
+
+    def __repr__(self) -> str:
+        return f"({self.data}) -> {self.next}"
--- tests/structures_tests/linked_list_test.py
+++ tests/structures_tests/linked_list_test.py
@@ -1,7 +1,7 @@
 from src.structures.linked_list import LinkedList
 
 
-def test_linked_list_node():
+def test_remove_last():
     lst = LinkedList.from_list(list(range(5)))
     assert str(lst) == "(0) -> (1) -> (2) -> (3) -> (4) -> None"
     removed = lst.remove_last()
