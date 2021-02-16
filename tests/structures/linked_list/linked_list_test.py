e7a8488e8c30f64eb915b52c989b7c8ed8c986e0
Tyler Yep <tyep@cs.stanford.edu>
2021-02-15 22:53:32 -0800

    linked list tests

--- tests/structures/linked_list/linked_list_test.py
+++ tests/structures/linked_list/linked_list_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from cs.structures import LinkedList
 
 
@@ -5,12 +7,54 @@ class TestLinkedList:
     @staticmethod
     def test_insert() -> None:
         lst = LinkedList[int]()
+        assert bool(lst) is False
         for i in range(10):
             lst.insert(i)
 
+        assert bool(lst) is True
         assert len(lst) == 10
         for i in range(10):
             assert i in lst
+
+        assert 13 not in lst
+        with pytest.raises(Exception):
+            lst.insert(13, 20)
+
+    @staticmethod
+    def test_indexing() -> None:
+        lst = LinkedList[int]()
+        with pytest.raises(Exception):
+            lst[0] = 5
+        with pytest.raises(Exception):
+            _ = lst[0]
+
+        for i in range(10):
+            lst.insert(i)
+
+        with pytest.raises(Exception):
+            _ = lst[20]
+        with pytest.raises(Exception):
+            lst[20] = 20
+
+        lst[7] = 8
+        assert lst[7] == 8
+
+    @staticmethod
+    def test_remove() -> None:
+        lst = LinkedList[int]()
+        with pytest.raises(Exception):
+            lst.remove(7)
+
+        for i in range(10):
+            lst.add_to_end(i)
+        lst.remove(0)
+        lst.remove(6)
+
+        for i in range(10):
+            lst.insert(i, i // 2)
+        lst.remove(6)
+        with pytest.raises(Exception):
+            lst.remove(6)
 
     @staticmethod
     def test_remove_last() -> None:
